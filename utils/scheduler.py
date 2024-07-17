c55589c96a0458bf499a8de31b180fa491c02e10
makcimerrr <maximedubs@proton.me>
2024-07-17 18:33:15 +0200

    :star: add schedule for cdi

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from cogs.gestion_jobs import JobCog  # Assurez-vous que JobCog est importé
+from cogs.gestion_jobs import JobCog
+from cogs.gestion_cdi import CDICog
 
 scheduler = AsyncIOScheduler()
 
@@ -16,14 +17,28 @@ async def joblist_evening():
     print("Updated jobs list auto 2!")
 
 
+async def cdi_morning():
+    cdi_cog = CDICog()  # Instanciez votre cog
+    await cdi_cog.send_cdilist()  # Appelez la méthode send_joblist
+    print("Updated jobs list auto 1!")
+
+
+async def cdi_evening():
+    cdi_cog = CDICog()  # Instanciez votre cog
+    await cdi_cog.send_cdilist()  # Appelez la méthode send_joblist
+    print("Updated jobs list auto 2!")
+
+
 @scheduler.scheduled_job("cron", hour=8, minute=0)  # Exécuter à 8h du matin
 async def schedule_joblist_morning():
     await joblist_morning()
+    await cdi_morning()
 
 
 @scheduler.scheduled_job("cron", hour=14, minute=10)  # Exécuter à 14h
 async def schedule_joblist_evening():
     await joblist_evening()
+    await cdi_evening()
 
 
 # Démarrer le scheduler
