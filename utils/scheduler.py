8705bb6bf86324c44ef474ad1e4d1a60a8c56e9d
mdubois <maxou.dub76@gmail.com>
2024-09-18 19:42:19 +0200

    :rocket: change hours scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -31,12 +31,12 @@ def start_scheduler(bot):
     # Print the time of the scheduler start
     print(f"Scheduler started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     # Scheduler jobs with bot parameter
-    @scheduler.scheduled_job("cron", hour=19, minute=38)  # Run at 9 AM
+    @scheduler.scheduled_job("cron", hour=9, minute=0)  # Run at 9 AM
     async def schedule_joblist_morning():
         await joblist_morning(bot)
         await cdi_morning(bot)
 
-    @scheduler.scheduled_job("cron", hour=19, minute=49)  # Run at 6 PM
+    @scheduler.scheduled_job("cron", hour=18, minute=0)  # Run at 6 PM
     async def schedule_joblist_evening():
         await joblist_evening(bot)
         await cdi_evening(bot)
