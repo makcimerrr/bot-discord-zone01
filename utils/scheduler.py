9ab8d25fad4548b1a525bdd78e1323611daf1edb
makcimerrr <maximedubs@proton.me>
2026-01-14 15:53:52 +0100

    refactor: uncomment job list update methods for full-time positions

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -31,16 +31,14 @@ async def joblist_evening(bot):
     print("Updated list internship auto 2!")
 
 
-""""async def cdi_morning(bot):
-    await send_cdilist(bot)  # Appelez la méthode send_joblist
+async def cdi_morning(bot):
+    await send_cdilist(bot)
     print("Updated list full-time auto 1!")
-"""
 
 
-"""async def cdi_evening(bot):
-    await send_cdilist(bot)  # Appelez la méthode send_joblist
+async def cdi_evening(bot):
+    await send_cdilist(bot)
     print("Updated list full-time auto 2!")
-"""
 
 
 
@@ -52,12 +50,12 @@ def start_scheduler(bot):
     @scheduler.scheduled_job("cron", hour=9, minute=0)  # Run at 9 AM
     async def schedule_joblist_morning():
         await joblist_morning(bot)
-        # await cdi_morning(bot)
+        await cdi_morning(bot)
 
     @scheduler.scheduled_job("cron", hour=18, minute=0)  # Run at 6 PM
     async def schedule_joblist_evening():
         await joblist_evening(bot)
-        # await cdi_evening(bot)
+        await cdi_evening(bot)
 
     @scheduler.scheduled_job("cron", hour=9, minute=0)  # Run at 9 AM
     async def schedule_fetch_progress_morning():
